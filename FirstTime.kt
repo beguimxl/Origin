eb5a4c032340a85ff7d1695ce048cbb8ce1a1bf2
beguimxl <banthehooks@yahoo.com>
2019-03-01 10:08:47 +0800

    Update FirstTime.kt

--- FirstTime.kt
+++ FirstTime.kt
@@ -1,21 +1,21 @@
 fun main(args: Array<String>) {
-    var a = 5
+	var a = 5
 	var b = 2;
-    var English = 90; 
-	var History = 80; 
-	var Music = 80; 
+	var english = 90; 
+	var history = 80; 
+	var music = 80; 
     
 	println("Bilguun Soninbayar, 2016089, March 1st, 2019")
-    println("I Love Coding.")
-    println("~~~~~~~~~~~~~~~~~")
+	println("I Love Coding.")
+	println("~~~~~~~~~~~~~~~~~")
     
-    println("5x2="+a*b) 
-    println("~~~~~~~~~~~~~~~~~")
+	println("5x2="+a*b) 
+	println("~~~~~~~~~~~~~~~~~")
     
-   	println("My Grades:")
-    println("English="+English)
-    println("History="+History)
-    println("Music="+Music)
-   	println("Total:"+(English+History+Music))
-   	println("Average:"+(English+History+Music)/3)   
+	println("My Grades:")
+	println("English="+english)
+	println("History="+history)
+	println("Music="+music)
+	println("Total:"+(english+history+music))
+	println("Average:"+(english+history+music)/3)   
 }
