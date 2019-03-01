c2031be42c87f8b8dd1938761cda6a225640f03c
Bilguun.Soninbayar <banthehooks@yahoo.com>
2019-03-01 09:29:29 +0800

    First commit

---
+++ FirstTime.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    var a = 5
+	var b = 2;
+    var English = 90; 
+	var History = 80; 
+	var Music = 80; 
+    
+	println("Bilguun Soninbayar, 2016089, March 1st, 2019")
+    println("I Love Coding.")
+    println("~~~~~~~~~~~~~~~~~")
+    
+    println("5x2="+a*b) 
+    println("~~~~~~~~~~~~~~~~~")
+    
+   	println("My Grades:")
+    println("English="+English)
+    println("History="+History)
+    println("Music="+Music)
+   	println("Total:"+(English+History+Music))
+   	println("Average:"+(English+History+Music)/3)   
+}
