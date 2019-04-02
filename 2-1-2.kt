29f780ffadcbfc2f48945332779d6917f4fdf0c9
beguimxl <banthehooks@yahoo.com>
2019-04-02 08:44:55 +0800

    Add files via upload

---
+++ 2-1-2.kt
@@ -0,0 +1,17 @@
+/* Bilguun Soninbayar -2016089- April 2nd */
+	
+	fun main(args: Array<String>) {
+    var x = 52
+	var y = 87
+    
+	println("Finding Bigger Number")
+	println("x = 52 ,y = 87")
+	
+	if(x >= y){
+		println("x $x is more")
+    }
+    
+    else{
+		println("y $y is more")
+    }
+}
