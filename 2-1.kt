c1af71fa3c0ab4e9913fc00bfdf83fb297558c50
beguimxl <banthehooks@yahoo.com>
2019-04-02 08:41:01 +0800

    Update 2-1.kt

--- 2-1.kt
+++ 2-1.kt
@@ -1,26 +1,26 @@
 fun main(args: Array<String>) {
   
-    var cm = 50
-    var cm1 = 180
-    var cm2 = 1500
-    var cm3 = 15000
-    var m: Int
+	var cm = 50
+	var cm1 = 180
+        var cm2 = 1500
+        var cm3 = 15000
+        var m: Int
     
-    println("Conversion of cm to m:")
+        println("Conversion of cm to m:")
 
-    m = cm  / 100
-    cm = cm % 100
-    println("$m m $cm cm")
+        m = cm  / 100
+        cm = cm % 100
+    	println("$m m $cm cm")
 
-    m = cm1  / 100
-    cm1 = cm1 % 100
-    println("$m m $cm1 cm")
+    	m = cm1  / 100
+        cm1 = cm1 % 100
+    	println("$m m $cm1 cm")
 
-	m = cm2  / 100
-    cm2 = cm2 % 100
-    println("$m m $cm2 cm")
+    	m = cm2  / 100
+    	cm2 = cm2 % 100
+    	println("$m m $cm2 cm")
 
-    m = cm3  / 100
-    cm3 = cm3 % 100
-    println("$m m $cm3 cm")
+    	m = cm3  / 100
+    	cm3 = cm3 % 100
+    	println("$m m $cm3 cm")
 }
