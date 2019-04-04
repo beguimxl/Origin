3639882b8efa54f584ddcc9834a8b931fe990bc9
beguimxl <banthehooks@yahoo.com>
2019-04-04 18:25:28 +0800

    Update 2-1-3.kt

--- 2-1-3.kt
+++ 2-1-3.kt
@@ -5,36 +5,35 @@
 	var y = 2
 	var z = 3
         
-    println("Finding Bigger Number")
+    	println("Finding Bigger Number")
     
 	fun comparison(){
-        
-	if(x > y && x > z){
-		println("x=$x is more than y=$y and z=$z")
+    		if(x > y && x > z){
+			println("x=$x is more than y=$y and z=$z")
+    		}
+    		if(y > x && y > z){
+			println("y=$y is more than x=$x and z=$z")
+    		}
+   		if(z > x && z > y){
+        		println("z=$z is more than x=$x and y=$y")  
+    		}
     	}
-    if(y > x && y > z){
-		println("y=$y is more than x=$x and z=$z")
-    	}
-    if(z > x && z > y){
-        println("z=$z is more than x=$x and y=$y")  
-    	}
-    }
     
-    comparison()
-    x = 1
-    y = 10
-    z = 2
-    comparison()
-    x = 20
-    y = 150 
-    z = 50
-    comparison()    
-    x = 50  
-    y = 1
-    z = 100 
-    comparison()
-    x = 200
-    y = 20
-    z = 15
-    comparison()
+    	comparison()
+    	x = 1
+    	y = 10
+    	z = 2
+    	comparison()
+    	x = 20
+    	y = 150 
+    	z = 50
+    	comparison()    
+    	x = 50  
+    	y = 1
+    	z = 100 
+    	comparison()
+    	x = 200
+    	y = 20
+    	z = 15
+    	comparison()
 }
