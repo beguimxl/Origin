e9460709113e12b5f553921c0f23d6408072e893
beguimxl <banthehooks@yahoo.com>
2019-04-04 20:32:46 +0800

    Update 3-2.kt

--- 3-2.kt
+++ 3-2.kt
@@ -3,6 +3,9 @@
     fun main(args:Array<String>) {
 	val superheroes = arrayOf("Superman", "Spider man", "Wonder woman", "Thor", "Black Panther", "Batman", "Cat", "Invisible Woman", "Iron man", "Hulk", "Aquaman")
 	println("List of all Superheroes:")
-	for(i in superheroes) println(i)
-	println("My favorite hero is: ${superheroes[1]}")
+	    
+	for(i in superheroes){
+		println(i)
 	}
+	println("My favorite hero is: ${superheroes[1]}")
+    }
